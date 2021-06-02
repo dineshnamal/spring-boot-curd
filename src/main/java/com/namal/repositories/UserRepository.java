3cf46787d694b9f60c3ad0a3ba75014a25a3e350
Namal Kankanamalage <namal@tramada.com>
2021-06-03 05:51:38 +1000

    first commit

---
+++ src/main/java/com/namal/repositories/UserRepository.java
@@ -0,0 +1,13 @@
+package com.namal.repositories;
+
+import com.namal.model.User;
+import java.util.List;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends CrudRepository<User, Long> {
+    
+    List<User> findByName(String name);
+    
+}
